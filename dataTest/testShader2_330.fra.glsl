02373f6d8583e69e65953d23d4d1622291f45891
Marcus Welander <mwthinker@yahoo.se>
2019-07-12 22:56:45 +0200

    Fix shader 330 bug

--- dataTest/testShader2_330.fra.glsl
+++ dataTest/testShader2_330.fra.glsl
@@ -8,11 +8,13 @@ in vec2 vTex;
 in vec4 vColor;
 in float vTexture;
 
+out vec4 OutColor;
+
 void main() {
 	if (vTexture > 0.5) {
-		vec4 c = texture2D(uTexture, vTex); 
-		gl_FragColor = vec4(vColor.r * c.r, vColor.g * c.g, vColor.b * c.b, vColor.a * c.a);
+		vec4 c = texture2D(uTexture, vTex);
+		OutColor = vec4(vColor.r * c.r, vColor.g * c.g, vColor.b * c.b, vColor.a * c.a);
 	} else {
-		gl_FragColor = vColor;
+		OutColor = vColor;
 	}
 }
